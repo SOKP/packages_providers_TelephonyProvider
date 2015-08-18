CyanogenMod/android_packages_providers_TelephonyProvider

[MMS-55]: Add new column when creating a new database and table or upgrade existing table.
Change-Id: Iecd5b15cfb5304827a5c1589516762c3abf7e5af

src/com/android/providers/telephony/MmsSmsDatabaseHelper.java
@@ -44,6 +44,7 @@ import android.provider.Telephony.Mms.Addr;
 import android.provider.Telephony.Mms.Part;
 import android.provider.Telephony.Mms.Rate;
 import android.provider.Telephony.MmsSms.PendingMessages;
+import android.provider.Telephony.ThreadsColumns;
 import android.telephony.SubscriptionManager;
 import android.util.Log;
 
@@ -163,12 +164,42 @@ public class MmsSmsDatabaseHelper extends SQLiteOpenHelper {
                         "     AND part.mid = pdu._id);" +
                         " END";
 
+    private static final String TRIGGER_GET_UNREAD_COUNT_ON_INSERT =
+            "CREATE TRIGGER update_unread_count_on_insert_sms " +
+            " AFTER INSERT ON sms " +
+            " BEGIN " +
+            "  UPDATE threads SET " +
+                 ThreadsColumns.UNREAD_MESSAGE_COUNT + "=(" +
+            "      SELECT count(_id) FROM sms WHERE read = 0" +
+            "    );" +
+            " END";
+
+    private static final String TRIGGER_GET_UNREAD_COUNT_ON_UPDATE =
+            "CREATE TRIGGER update_unread_count_on_update_sms " +
+            " AFTER UPDATE ON sms " +
+            " BEGIN " +
+            "  UPDATE threads SET " +
+                 ThreadsColumns.UNREAD_MESSAGE_COUNT + "=(" +
+            "      SELECT count(_id) FROM sms WHERE read = 0" +
+            "    );" +
+            " END";
+
+    private static final String TRIGGER_GET_UNREAD_COUNT_ON_DELETE =
+            "CREATE TRIGGER update_unread_count_on_delete_sms" +
+            " AFTER DELETE ON sms " +
+            " BEGIN " +
+            "  UPDATE threads SET " +
+                 ThreadsColumns.UNREAD_MESSAGE_COUNT + "=(" +
+            "      SELECT count(_id) FROM sms WHERE read = 0" +
+            "    );" +
+            " END";
+
     private static MmsSmsDatabaseHelper sInstance = null;
     private static boolean sTriedAutoIncrement = false;
     private static boolean sFakeLowStorageTest = false;     // for testing only
 
     static final String DATABASE_NAME = "mmssms.db";
-    static final int DATABASE_VERSION = 64;
+    static final int DATABASE_VERSION = 65;
     private final Context mContext;
     private LowStorageMonitor mLowStorageMonitor;
 
@@ -177,7 +208,6 @@ public class MmsSmsDatabaseHelper extends SQLiteOpenHelper {
 
     private MmsSmsDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-
         mContext = context;
     }
 
@@ -345,14 +375,14 @@ public class MmsSmsDatabaseHelper extends SQLiteOpenHelper {
 
         try {
             db.execSQL(
-            "  UPDATE threads" +
-            "  SET" +
-            "  date =" +
-            "    (SELECT date FROM" +
-            "        (SELECT date * 1000 AS date, thread_id FROM pdu" +
-            "         UNION SELECT date, thread_id FROM sms)" +
-            "     WHERE thread_id = " + thread_id + " ORDER BY date DESC LIMIT 1)" +
-            "  WHERE threads._id = " + thread_id + ";");
+                    "  UPDATE threads" +
+                            "  SET" +
+                            "  date =" +
+                            "    (SELECT date FROM" +
+                            "        (SELECT date * 1000 AS date, thread_id FROM pdu" +
+                            "         UNION SELECT date, thread_id FROM sms)" +
+                            "     WHERE thread_id = " + thread_id + " ORDER BY date DESC LIMIT 1)" +
+                            "  WHERE threads._id = " + thread_id + ";");
         } catch (Throwable ex) {
             Log.e(TAG, ex.getMessage(), ex);
         }
@@ -815,6 +845,9 @@ public class MmsSmsDatabaseHelper extends SQLiteOpenHelper {
                    "  UPDATE threads SET error = error - 1" +
                    "  WHERE _id = OLD.thread_id; " +
                    "END;");
+        db.execSQL(TRIGGER_GET_UNREAD_COUNT_ON_INSERT);
+        db.execSQL(TRIGGER_GET_UNREAD_COUNT_ON_UPDATE);
+        db.execSQL(TRIGGER_GET_UNREAD_COUNT_ON_DELETE);
     }
 
     private void createSmsTables(SQLiteDatabase db) {
@@ -907,6 +940,7 @@ public class MmsSmsDatabaseHelper extends SQLiteOpenHelper {
                    Threads._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
                    Threads.DATE + " INTEGER DEFAULT 0," +
                    Threads.MESSAGE_COUNT + " INTEGER DEFAULT 0," +
+                   Threads.UNREAD_MESSAGE_COUNT + " INTEGER DEFAULT 0," +
                    Threads.RECIPIENT_IDS + " TEXT," +
                    Threads.SNIPPET + " TEXT," +
                    Threads.SNIPPET_CHARSET + " INTEGER DEFAULT 0," +
@@ -1400,6 +1434,22 @@ public class MmsSmsDatabaseHelper extends SQLiteOpenHelper {
                 db.endTransaction();
             }
             return;
+        case 64:
+            if (currentVersion <= 64) {
+                return;
+            }
+
+            db.beginTransaction();
+            try {
+                upgradeDatabaseToVersion65(db);
+                db.setTransactionSuccessful();
+            } catch (Throwable ex) {
+                Log.e(TAG, ex.getMessage(), ex);
+                break;
+            } finally {
+                db.endTransaction();
+            }
+            return;
         }
 
         Log.e(TAG, "Destroying all old data.");
@@ -1695,6 +1745,24 @@ public class MmsSmsDatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("UPDATE " + MmsProvider.TABLE_PDU + " SET " + Mms.SUBSCRIPTION_ID + " = -1");
         db.execSQL("UPDATE " + SmsProvider.TABLE_SMS + " SET " + Sms.SUBSCRIPTION_ID + " = -1");
         db.execSQL("UPDATE " + SmsProvider.TABLE_RAW + " SET " + Sms.SUBSCRIPTION_ID + " = -1");
+    }
+
+    private void upgradeDatabaseToVersion65(SQLiteDatabase db) {
+
+        try {
+            db.execSQL("ALTER TABLE " + MmsSmsProvider.TABLE_THREADS + " ADD COLUMN "
+            + Threads.UNREAD_MESSAGE_COUNT + " INTEGER DEFAULT 0");
+        } catch (SQLiteException e) {
+            // consume since we have nothing to do
+        }
+
+        // Add triggers to keep record up to date
+        db.execSQL(TRIGGER_GET_UNREAD_COUNT_ON_INSERT);
+        db.execSQL(TRIGGER_GET_UNREAD_COUNT_ON_UPDATE);
+        db.execSQL(TRIGGER_GET_UNREAD_COUNT_ON_DELETE);
+
+        // [TODO][MSB]: Figure out how to populate initial values?
+
     }
 
     // Try to copy data from existing src column to new column which supposed

SOKP/packages_providers_TelephonyProvider

src/com/android/providers/telephony/MmsSmsDatabaseHelper.java
@@ -163,12 +163,45 @@ public class MmsSmsDatabaseHelper extends SQLiteOpenHelper {
                         "     AND part.mid = pdu._id);" +
                         " END";
 
+    private static final String TRIGGER_GET_UNREAD_COUNT_ON_INSERT =
+            "CREATE TRIGGER update_unread_count_on_insert_sms " +
+            " AFTER INSERT ON sms " +
+            " BEGIN " +
+            "  UPDATE threads SET " +
+                 Threads.UNREAD_MESSAGE_COUNT + "=(" +
+            "      SELECT count(_id) FROM sms WHERE read = 0" +
+            "          AND thread_id = new.thread_id" +
+            "    ) WHERE threads._id = new.thread_id;" +
+            " END";
+
+    private static final String TRIGGER_GET_UNREAD_COUNT_ON_UPDATE =
+            "CREATE TRIGGER update_unread_count_on_update_sms " +
+            " AFTER UPDATE ON sms " +
+            " BEGIN " +
+            "  UPDATE threads SET " +
+                 Threads.UNREAD_MESSAGE_COUNT + "=(" +
+            "      SELECT count(_id) FROM sms WHERE read = 0" +
+            "          AND thread_id = new.thread_id" +
+            "    ) WHERE threads._id = new.thread_id;" +
+            " END";
+
+    private static final String TRIGGER_GET_UNREAD_COUNT_ON_DELETE =
+            "CREATE TRIGGER update_unread_count_on_delete_sms" +
+            " AFTER DELETE ON sms " +
+            " BEGIN " +
+            "  UPDATE threads SET " +
+                 Threads.UNREAD_MESSAGE_COUNT + "=(" +
+            "      SELECT count(_id) FROM sms WHERE read = 0" +
+            "          AND thread_id = old.thread_id" +
+            "    ) WHERE threads._id = old.thread_id;" +
+            " END";
+
     private static MmsSmsDatabaseHelper sInstance = null;
     private static boolean sTriedAutoIncrement = false;
     private static boolean sFakeLowStorageTest = false;     // for testing only
 
     static final String DATABASE_NAME = "mmssms.db";
-    static final int DATABASE_VERSION = 64;
+    static final int DATABASE_VERSION = 65;
     private final Context mContext;
     private LowStorageMonitor mLowStorageMonitor;
 
@@ -815,6 +848,9 @@ public class MmsSmsDatabaseHelper extends SQLiteOpenHelper {
                    "  UPDATE threads SET error = error - 1" +
                    "  WHERE _id = OLD.thread_id; " +
                    "END;");
+        db.execSQL(TRIGGER_GET_UNREAD_COUNT_ON_INSERT);
+        db.execSQL(TRIGGER_GET_UNREAD_COUNT_ON_UPDATE);
+        db.execSQL(TRIGGER_GET_UNREAD_COUNT_ON_DELETE);
     }
 
     private void createSmsTables(SQLiteDatabase db) {
@@ -914,7 +950,9 @@ public class MmsSmsDatabaseHelper extends SQLiteOpenHelper {
                    Threads.ARCHIVED + " INTEGER DEFAULT 0," +
                    Threads.TYPE + " INTEGER DEFAULT 0," +
                    Threads.ERROR + " INTEGER DEFAULT 0," +
-                   Threads.HAS_ATTACHMENT + " INTEGER DEFAULT 0);");
+                   Threads.HAS_ATTACHMENT + " INTEGER DEFAULT 0," +
+                   Threads.UNREAD_MESSAGE_COUNT + " INTEGER DEFAULT 0);"
+        );
 
         /**
          * This table stores the queue of messages to be sent/downloaded.
@@ -1399,6 +1437,22 @@ public class MmsSmsDatabaseHelper extends SQLiteOpenHelper {
             } finally {
                 db.endTransaction();
             }
+            // fall through
+        case 64:
+            if (currentVersion <= 64) {
+                return;
+            }
+
+            db.beginTransaction();
+            try {
+                upgradeDatabaseToVersion65(db);
+                db.setTransactionSuccessful();
+            } catch (Throwable ex) {
+                Log.e(TAG, ex.getMessage(), ex);
+                break;
+            } finally {
+                db.endTransaction();
+            }
             return;
         }
 
@@ -1695,6 +1749,22 @@ public class MmsSmsDatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("UPDATE " + MmsProvider.TABLE_PDU + " SET " + Mms.SUBSCRIPTION_ID + " = -1");
         db.execSQL("UPDATE " + SmsProvider.TABLE_SMS + " SET " + Sms.SUBSCRIPTION_ID + " = -1");
         db.execSQL("UPDATE " + SmsProvider.TABLE_RAW + " SET " + Sms.SUBSCRIPTION_ID + " = -1");
+    }
+
+    private void upgradeDatabaseToVersion65(SQLiteDatabase db) {
+
+        try {
+            db.execSQL("ALTER TABLE " + MmsSmsProvider.TABLE_THREADS + " ADD COLUMN "
+            + Threads.UNREAD_MESSAGE_COUNT + " INTEGER DEFAULT 0");
+        } catch (SQLiteException e) {
+            // consume since we have nothing to do
+        }
+
+        // Add triggers to keep record up to date
+        db.execSQL(TRIGGER_GET_UNREAD_COUNT_ON_INSERT);
+        db.execSQL(TRIGGER_GET_UNREAD_COUNT_ON_UPDATE);
+        db.execSQL(TRIGGER_GET_UNREAD_COUNT_ON_DELETE);
+
     }
 
     // Try to copy data from existing src column to new column which supposed